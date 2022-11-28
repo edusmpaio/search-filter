b99a477041aa321d398e89013a5bc7e73efb0832
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-11-28 15:35:12 -0300

    finished header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="h-screen bg-zinc-800">
+    <div className="h-screen bg-zinc-900">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
+import { BiSearch } from "react-icons/bi";
+
 function Header() {
   return (
-    <header className="max-w-md mx-auto pt-10 px-3">
+    <header className="max-w-md mx-auto py-10 px-3">
       <h1 className="text-3xl font-bold text-center mb-4">
         What are you looking for?
       </h1>
-      <div>
+      <div className="relative">
         <input
           type="text"
           placeholder="Filter"
-          className="w-full px-4 py-2 rounded-lg"
+          className="w-full px-4 py-2.5 rounded-lg bg-zinc-700"
         />
+        <BiSearch size={20} className="absolute bottom-3 right-3" />
       </div>
     </header>
   );
